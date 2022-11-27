ff972371d92b0c22521a52d0cc52c8c8fe8f1572
marisabel <marisabelfernandez174@gmail.com>
2022-11-27 00:48:39 -0300

    primera parte de proyecto

---
+++ app/proceso_cuentas.py
@@ -0,0 +1,18 @@
+import csv 
+from persona import Persona 
+
+
+def crear_cuentas(): 
+
+    personas = {} 
+    archivo = open("personas.csv", "r") 
+    archivo_csv = csv.reader(archivo) 
+    next(archivo_csv) 
+    for nombre, dni, fecha_nacimiento in archivo_csv: 
+        persona = Persona(dni, nombre, fecha_nacimiento) 
+        persona.crear_cuenta() 
+        personas['dni'] = persona 
+
+    archivo.close() 
+    print(f' {personas}') 
+    return personas     
